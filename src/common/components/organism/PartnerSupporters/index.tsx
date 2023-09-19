e6e968d6baa5515dd6e66b63c85ed94adb46f97b
xdefidev <xdefidev@gmail.com>
2023-09-19 16:40:35 +0100

    ch

--- src/common/components/organism/PartnerSupporters/index.tsx
+++ src/common/components/organism/PartnerSupporters/index.tsx
@@ -7,7 +7,7 @@ export default function PartnerSupporters() {
         <Box marginBottom={6} className='bg-[#1F2A45] py-8 px-2 rounded-[10px]' marginTop={10}>
             <Box component='a' id='partners' className='relative xs:-top-[150px] lg:-top-[80px]'></Box>
             <Stack className='flex justify-center'>
-                <Typography variant='h4' className='font-bold text-center mb-4'>Partners & Supporters</Typography>
+                <Typography variant='h4' className='font-bold text-center mb-4'>Partners</Typography>
                 <Grid container columnSpacing={4} rowSpacing={0} alignItems='center' justifyContent='center' paddingX={6}>
                     <Grid item xs={12} md={3}>
                         <Image src='/sponsors/ethereum.png' width={100} height={100} alt='Alcide Logo' layout='responsive' className='hover:brightness-110 aspect-video object-contain hover:scale-105 transition' />
